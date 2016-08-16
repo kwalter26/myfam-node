b6fb46ee827d2ed1da94d6a4fd7c29b8ac50c245
Kyle Walter <walter.kl26@gmail.com>
2016-08-15 23:10:43 -0500

    Started creating chat room.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,4 +1,15 @@
-var socket = io('//localhost:3000');
-socket.on('socketToMe', function (data) {
-  $('.chatroom').append(data + '\n');
-});
+
+angular.module('app', [])
+  .controller('main',function($scope){
+    $scope.messages = '';
+    $scope.message = 'uh oh';
+    $scope.addMessage = function(){
+
+
+    }
+
+  var socket = io('//localhost:3000');
+  socket.on('socketToMe', function (data) {
+    $scope.messages += ('<' + new Date().toLocaleTimeString() + '> '+ data + '\n');
+  });
+})
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/send',function(req,res,next){
+  res.io.emit('message', 'users');
+});
+
 module.exports = router;
